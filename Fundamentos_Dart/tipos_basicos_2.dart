8bcc4a8a27b49affdff68e2643cfe8dd8ca59f5f
Fabiano Muscardi Muniz Junior <fabiano.junior@araujo.com.br>
2021-02-28 19:33:27 -0300

    Adicionado tipos_basicos_2.dart

---
+++ Fundamentos_Dart/tipos_basicos_2.dart
@@ -0,0 +1,44 @@
+/* 
+
+  - List
+  - Set
+  - Map
+*/
+
+main() {
+  // List
+  var aprovados = ['Ana', 'Carlos', 'Daniel', 'Rafael'];
+  aprovados.add('Daniel');
+  print(aprovados is List);
+  print(aprovados);
+  print(aprovados.elementAt(2));
+  print(aprovados[0]);
+  print(aprovados.length);
+
+  // Map
+  var telefones = {
+    'João': '+55 (11)98765-4321',
+    'Maria': '+55 (11)95464-4321',
+    'Pedro': '+55 (11)99844-4321',
+  };
+
+  print(telefones is Map);
+  print(telefones);
+  print(telefones['João']);
+  print(telefones.length);
+  print(telefones.keys);
+  print(telefones.values);
+  print(telefones.entries);
+
+  // Conjunto
+  var times = {'Vasco', 'Flamengo', 'Fortaleza', 'São Paulo'};
+  print(times is Set);
+  times.add('Palmeiras');
+  times.add('Palmeiras');
+  times.add('Palmeiras');
+  print(times.length);
+  print(times.contains('Vasco'));
+  print(times.first);
+  print(times.last);
+  print(times);
+}
