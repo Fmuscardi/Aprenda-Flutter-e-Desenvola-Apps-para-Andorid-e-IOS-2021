025686894ca646e1a83a0f3786c5fe3778c02c92
Fabiano Muscardi Muniz Junior <fabiano.junior@araujo.com.br>
2021-02-28 21:22:54 -0300

    Adicionado generics.dart

---
+++ Fundamentos_Dart/generics.dart
@@ -0,0 +1,13 @@
+main() {
+  
+  List<String> frutas = ['Banana', 'Maça', 'Goiaba'];
+  frutas.add('Melão');
+  print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiário': 600.00
+  };
+  print(salarios);
+}
