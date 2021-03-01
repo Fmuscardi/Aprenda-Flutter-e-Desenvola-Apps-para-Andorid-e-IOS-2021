4813257b56714a0ac3153563714aee853a42ef55
Fabiano Muscardi Muniz Junior <fabiano.junior@araujo.com.br>
2021-03-01 11:32:45 -0300

    Desafio resolvido

--- Estrutura_De_Controle_Dart/desafio_if_else.dart
+++ Estrutura_De_Controle_Dart/desafio_if_else.dart
@@ -1,7 +1,9 @@
 main() {
   var nota = 2.3;
 
-  if (nota >= 9.0) {
-    print("Parabéns! Você é brilhante!");
+  if (nota >= 9.0) // Não usa ";" em estruturar de controle (Com exceção do/while)
+  {
+    print("Parabéns! Você é brilhante!"); 
   }
 }
+
