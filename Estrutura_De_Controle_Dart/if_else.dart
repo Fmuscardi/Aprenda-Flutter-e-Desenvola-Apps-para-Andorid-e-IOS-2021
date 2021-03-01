e8d4d241bf8ded82c6f34137f5f4d3af1eb7a040
Fabiano Muscardi Muniz Junior <fabiano.junior@araujo.com.br>
2021-02-28 21:40:37 -0300

    Criado o diretório Estrutura_De_Controle_Dart e Adicionado if_else.dart

---
+++ Estrutura_De_Controle_Dart/if_else.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+main() {
+  var nota = Random().nextInt(11);
+  print("Nota slecionada: $nota.");
+
+  if (nota >= 9) {
+    print('Quadro de honra!');
+  } else if (nota >= 7) {
+    print('Recuperação!');
+  } else if (nota >= 5) {
+    print('Recuperação!');
+  } else if (nota >= 4) {
+    print('Recuperação + Trabalho!');
+  } else {
+    print('Reprovado!');
+  }
+
+  if (nota >= 7) {
+    print("Aprovado!");
+  } else if (nota >= 5) {
+    print("Recuperação!");
+  } else {
+    print("Reprovado!");
+  }
+}
